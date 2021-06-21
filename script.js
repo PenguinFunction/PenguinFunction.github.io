2faa80053da2cc3beb9a293dbdeb3ce83c7b5db1
PenguinFunction <63715328+PenguinFunction@users.noreply.github.com>
2021-06-21 16:51:16 +1200

    Added introduction content
    - Refined header
    - Refined document layout

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 window.onload = function () {
-    removeStartAnimationDivs();
+    // Disable scrolling on startup
+    document.body.style.overflowY = "hidden";
+    document.getElementsByTagName("header")[0].style.position = "fixed";
+
+    removeStartupAnimDivs();
+    document.getElementById("light-dark-toggle").addEventListener("click", function () { toggleLightDarkMode() });
 }
 
-function removeStartAnimationDivs() {
+function removeStartupAnimDivs() {
     setTimeout(() => {
+        document.body.style.overflowY = "initial";
+        document.getElementsByTagName("header")[0].style.position = "inherit";
         var animContainer = document.getElementById("start-animation-container");
         animContainer.parentNode.removeChild(animContainer);
     }, 4500);
+}
+
+function toggleLightDarkMode() {
+    console.log("Test");
 }
